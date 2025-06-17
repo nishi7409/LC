e6f8c1ea2e5a9720308d3a240191cf2bc0f1bfc0
Nishant Srivastava <srivan@rpi.edu>
2025-06-16 22:51:59 -0400

    feat: add python3 solution for two-sum

---
+++ two-sum/two-sum.py
@@ -0,0 +1,8 @@
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        history = dict()
+        for index, number in enumerate(nums):
+            if target-number in history:
+                return [history[target-number], index]
+            history[number] = index
