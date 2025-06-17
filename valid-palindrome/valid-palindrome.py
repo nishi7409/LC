6ea712ada0805bcc30ab7cb8ff1469cc0702bafb
Nishant Srivastava <srivan@rpi.edu>
2025-06-17 19:54:58 -0400

    feat: add python3 solution for valid-palindrome

---
+++ valid-palindrome/valid-palindrome.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        result = ""
+        for char in s:
+            if char.isalnum():
+                result += char.lower()
+        
+        return result == result[::-1]
