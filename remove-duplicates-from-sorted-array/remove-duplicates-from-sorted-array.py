8646de9b513e3416a949967ece691a2e59034806
Nishant Srivastava <srivan@rpi.edu>
2025-06-18 19:20:39 -0400

    feat: update python3 solution for remove-duplicates-from-sorted-array

--- remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -1,5 +1,12 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        sorted_stack = sorted(set(nums))
-        nums[:len(sorted_stack)] = sorted_stack
-        return len(sorted_stack)
+        if not nums:
+            return 0
+        
+        right = 1
+
+        for left in range(1, len(nums)):
+            if nums[left] != nums[left-1]:
+                nums[right] = nums[left]
+                right += 1
+        return right
