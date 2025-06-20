4df8e58be3c7acb9804229822e6eabf5b70912cc
Nishant Srivastava <srivan@rpi.edu>
2025-06-19 23:15:01 -0400

    feat: add python3 solution for middle-of-the-linked-list

---
+++ middle-of-the-linked-list/middle-of-the-linked-list.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+import math
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # brute force
+        temp = list()
+        curr = head
+        while curr:
+            temp.append(curr.val)
+            curr = curr.next
+        middle_length = math.ceil(len(temp)/2)
+
+        new_curr = head
+        if len(temp)%2 != 0:
+            for x in range(middle_length-1):
+                new_curr = new_curr.next
+        else:
+            for x in range(middle_length):
+                new_curr = new_curr.next
+        return new_curr
